f1934360faa68d7dac7b5d70a80cd185261603ed
Miroslav Belak <miroslavbelak123@gmail.com>
2022-09-09 15:02:36 +0200

    added redux persist

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import {PersistGate} from "redux-persist/integration/react"
 import App from './App';
-import { store } from './store/store';
+import { persistor, store } from './store/store';
 import './index.css';
 
 
@@ -11,9 +12,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor} >
     <BrowserRouter>
        <App />
     </BrowserRouter>
+    </PersistGate>
     </Provider>
   </React.StrictMode>
 );
